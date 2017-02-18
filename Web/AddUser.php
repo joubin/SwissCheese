606824411a7ca2a2afc5cfca4de19f6109e7ef07
Joubin Jabbari <joubin.j@gmail.com>
2017-02-18 15:24:35 -0800

    test

---
+++ Web/AddUser.php
@@ -0,0 +1,9 @@
+<?php
+include_once('TestConnection.php');
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
--- Web/TestConnection.php
+++ Web/TestConnection.php
@@ -6,9 +6,4 @@ $password = "cheese";
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
 ?>
