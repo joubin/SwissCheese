7d3552e653f6d21b8187f0e5b5f284d7f7c4057b
Joubin Jabbari <joubin.j@gmail.com>
2017-02-19 15:42:24 -0800

    pushin

--- Web/index.php
+++ Web/index.php
@@ -1,11 +1,5 @@
 <?php
 include_once('TestConnection.php');
-
-// // Check connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-// echo "Connected successfully";
 ?>
 <!DOCTYPE html>
 <html lang="en">
