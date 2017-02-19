c52486f0ffae90a3b6daf35e98397114d94a466c
Joubin Jabbari <joubin.j@gmail.com>
2017-02-19 15:42:54 -0800

    pushin

--- Web/TestConnection.php
+++ Web/TestConnection.php
@@ -5,6 +5,7 @@ $password = "cheese";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
