3a7714882cb4488460934d27f2450bc15d361874
Joubin Jabbari <joubin.j@gmail.com>
2017-02-19 16:40:35 -0800

    pushin

--- Web/index.php
+++ Web/index.php
@@ -73,18 +73,26 @@
       if ($results = $conn->query("SELECT * FROM Comments")) {
         foreach ($results as $result) {
           $name = $result["name"];
+          $imageurl = $result["image"]
           $text = $result["text"];
           print <<<END
+          <div class="col-sm-1">
+          <div class="thumbnail">
+          <img class="img-responsive user-photo" src="$imageurl">
+          </div><!-- /thumbnail -->
+          </div><!-- /col-sm-1 -->
+
           <div class="col-sm-5">
           <div class="panel panel-default">
           <div class="panel-heading">
-          <strong>$name</strong>
+          <strong>$name</strong> <span class="text-muted">commented 5 days ago</span>
           </div>
           <div class="panel-body">
-            $text
+          $text
           </div><!-- /panel-body -->
           </div><!-- /panel panel-default -->
           </div><!-- /col-sm-5 -->
+          </div><!-- /row -->
 END;
 
           // printf($result['name']);
