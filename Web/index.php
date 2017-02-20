86e67faa899f8010137327cb57dc9f6f4e8a7936
Joubin Jabbari <joubin.j@gmail.com>
2017-02-19 20:04:32 -0800

    pushin

--- Web/index.php
+++ Web/index.php
@@ -6,7 +6,12 @@ $email = $_POST["email"];
 $text = $_POST["notes"];
 $image = $_POST["image"];
 
-$conn->query("insert into Comments values(NULL, $name, $email, $text, $image)");
+;
+if ($conn->query("insert into Comments values(NULL, $name, $email, $text, $image)") === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 ?>
 
 <!DOCTYPE html>
