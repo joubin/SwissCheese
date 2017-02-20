0607d581325e28d0e36ae4a101472c3e583d377f
Joubin Jabbari <joubin.j@gmail.com>
2017-02-19 16:19:59 -0800

    pushin

--- Web/index.php
+++ Web/index.php
@@ -72,7 +72,7 @@
       /* Select queries return a resultset */
       if ($results = $conn->query("SELECT * FROM Comments")) {
           foreach ($results as $result) {
-            echo $result;
+            print_r($result)
           }
       }else{
         printf("Failed to get anything");
