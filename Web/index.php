07c645aec0c640dda93d3d0f536c83bdf08f5fa0
Joubin Jabbari <joubin.j@gmail.com>
2017-02-19 17:01:15 -0800

    pushin

--- Web/index.php
+++ Web/index.php
@@ -81,7 +81,7 @@
             </div><!-- /thumbnail -->
           </div><!-- /col-sm-1 -->
 
-          <div class="col-sm-5" style="float:right;">
+          <div class="col-sm-5">
             <div class="panel panel-default">
               <div class="panel-heading">
                 <strong>$name</strong> <span class="text-muted">commented 5 days ago</span>
